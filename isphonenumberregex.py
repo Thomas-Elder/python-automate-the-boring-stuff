4d14d3a1b8f02346ae48c8dd89bb622d454fefe3
Thomas <thombsaway@gmail.com>
2020-07-03 10:11:45 +1000

    Amended regex to make area code optional

--- isphonenumberregex.py
+++ isphonenumberregex.py
@@ -2,10 +2,14 @@
 import sys
 import re
 
-phonepattern = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+# \d matches any numerical value
+# () can be used to group patterns
+# ? specifies that the preceding group appears once or not at all in the pattern
+# So the below will match 123-123-1234, and 123-1234
+phonepattern = re.compile(r'(\d\d\d-)?(\d\d\d-\d\d\d\d)')
 match = phonepattern.search(sys.argv[1])
 
 if match == None:
     print('Text entered is not a phone number')
 else:
-    print('Area code is %s and number is %s' % (match.group(1), match.group(2)))
+    print('Number is %s' % (match.group()))
