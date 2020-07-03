fa87e01c44cdade4a3d2de4e77f93c6975ea0170
Thomas <thombsaway@gmail.com>
2020-07-03 10:36:11 +1000

    Added examples for {x} specifying the number of patterns to match

--- regex.py
+++ regex.py
@@ -49,4 +49,38 @@ print(match)
 # None because wo doesn't appear in this string
 
 #
+# We can use {x} to specify that a pattern appear x times 
+br6 = re.compile(r'The adventures of bat(wo){3}man and (his|her) bat(mobile|copter)')
+match = br6.search('The adventures of batwowowoman and her batcopter')
+print(match)
+# <re.Match object; span=(0, 48), match='The adventures of batwowowoman and her batcopter'>
+
+br6 = re.compile(r'The adventures of bat(wo){3}man and (his|her) bat(mobile|copter)')
+match = br6.search('The adventures of batwowoman and her batcopter') # only two wo
+print(match)
+# None
+
 #
+# And we can use {x, y} to specify that a pattern appear x to y times 
+br6 = re.compile(r'The adventures of bat(wo){3,5}man and (his|her) bat(mobile|copter)')
+match = br6.search('The adventures of batwowowowowoman and her batcopter')
+print(match)
+# <re.Match object; span=(0, 48), match='The adventures of batwowowowowoman and her batcopter'>
+
+# This would work for batwowowoman, batwowowowoman, and batwowowowowoman
+# Just like slicing, we can leave off either number
+# {3,} means 3 or more, {,5} means 0 to 5.
+
+# Matching digits
+dr1 = re.compile(r'(\d){3,5}')
+match = dr1.search('123456789')
+print(match)
+# <re.Match object; span=(0, 5), match='12345'>
+# This is greedy matching, it matches the longest possible string that fits the pattern.
+# It also matches the first possible match. 
+# For a non-greedy match, you can add a ? after the curly braces
+dr2 = re.compile(r'(\d){3,5}?')
+match = dr2.search('123456789')
+print(match)
+# <re.Match object; span=(0, 3), match='123'>
+
