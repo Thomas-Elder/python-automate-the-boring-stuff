19370785077ac838bf8f835df08492a159eaced5
Thomas <thombsaway@gmail.com>
2020-07-09 11:09:41 +1000

    Created shelf, added data

--- shelf.py
+++ shelf.py
@@ -1,3 +1,11 @@
 #! python3
 
 import shelve
+
+# "A "shelf" is a persistent, dictionary-like object."
+# You can open it like a file, add to it like a dictionary.
+shelfFile = shelve.open('myShelf')
+shelfFile['exercises'] = ['snatch', 'clean and jerk', 'front squat', 'push press']
+
+print(shelfFile['exercises']) # ['snatch', 'clean and jerk', 'front squat', 'push press']
+shelfFile.close()
