f4e24e27c6082412006d195685b938c634ad387f
Thomas <thombsaway@gmail.com>
2020-07-10 15:26:36 +1000

    Adding raise exception examples

---
+++ raise.py
@@ -0,0 +1,22 @@
+#! python3
+
+def boxPrint(symbol, width, height):
+
+    if(len(symbol) != 1):
+        raise Exception('Symbol needs to be length of 1')
+
+    print(symbol*width)
+
+    for x in range(0, height - 2):
+        print(symbol + ' ' * (width - 2) + symbol)
+
+    print(symbol*width)
+
+boxPrint('*', 5, 5)
+boxPrint('t', 15, 5)
+boxPrint('tt', 15, 5) # Traceback (most recent call last):
+                      # File ".\raise.py", line 17, in <module>
+                      # boxPrint('tt', 15, 5)
+                      # File ".\raise.py", line 6, in boxPrint
+                      # raise Exception('Symbol needs to be length of 1')
+                      # Exception: Symbol needs to be length of 1
