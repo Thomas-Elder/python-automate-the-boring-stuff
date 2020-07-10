25b35ebae720400354b0c8807097d5cc53730cd5
Thomas <thombsaway@gmail.com>
2020-07-10 15:34:47 +1000

    Added handling exception example

--- raise.py
+++ raise.py
@@ -1,5 +1,7 @@
 #! python3
 
+import traceback
+
 def boxPrint(symbol, width, height):
 
     if(len(symbol) != 1):
@@ -14,9 +16,19 @@ def boxPrint(symbol, width, height):
 
 boxPrint('*', 5, 5)
 boxPrint('t', 15, 5)
-boxPrint('tt', 15, 5) # Traceback (most recent call last):
+#boxPrint('tt', 15, 5) # Traceback (most recent call last):
                       # File ".\raise.py", line 17, in <module>
                       # boxPrint('tt', 15, 5)
                       # File ".\raise.py", line 6, in boxPrint
                       # raise Exception('Symbol needs to be length of 1')
                       # Exception: Symbol needs to be length of 1
+
+# Fancy error handling
+try:
+    boxPrint('tt', 15, 5)
+except:
+    errorFile = open('.\\error_log.txt', 'a')
+    errorFile.write(traceback.format_exc())
+    errorFile.write('\n\n')
+    errorFile.close()
+    print('Exception handled and written to error_log.txt')
