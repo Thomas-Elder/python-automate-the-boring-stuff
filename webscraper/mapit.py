6312de9669978d564f3553bcbc97ab5800fedaf0
Thomas <thombsaway@gmail.com>
2020-07-13 15:05:33 +1000

    Started webscraper section
    Added new folder for related files.
    
    Created a script for launching a browser at google maps checking for command line arguments, or the clipboard for the location to search for.

---
+++ webscraper/mapit.py
@@ -0,0 +1,32 @@
+#! python3
+
+# Imports
+import webbrowser
+import sys
+import pyperclip
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+if len(sys.argv) > 1:
+    logging.debug('sys.argv > 1, location parameters entered.')
+    logging.debug('sys.argv: %s' % (sys.argv))
+
+    location = '+'.join(sys.argv[1:])
+    logging.debug('location: %s' % (location))
+
+    webbrowser.open('https://www.google.com.au/maps/place/%s' % (location))
+
+elif pyperclip.paste() == '':
+    logging.info('Clipboard is empty')
+
+    clipboard = pyperclip.paste()
+    logging.debug(clipboard)
+
+    location = '+'.join(clipboard.split(' '))
+    logging.debug(location)
+
+    webbrowser.open('https://www.google.com.au/maps/place/%s' % (location))
+
+else: 
+    logging.info('No parameters passed, and nothing on the clipboard.')
