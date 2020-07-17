cedbbd75255d858c7ab8e44ffeb73c5757ef4544
Thomas <thombsaway@gmail.com>
2020-07-17 20:33:48 +1000

    Refactored file retrieval into function

--- webscraper/xkcd.py
+++ webscraper/xkcd.py
@@ -7,8 +7,27 @@ import bs4
 import requests
 import urllib.request
 
-# logging
-logging.basicConfig(filename='..\\logs\\xkcd.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging - filename='..\\logs\\xkcd.log', 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def getImage(response):
+    response.raise_for_status()
+
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+    # Identify the div that contains the comic, by id='comic' ...
+    comicDiv = soup.find('div', {'id':'comic'})
+
+    # Then find the img tag within that div ... 
+    comicImg = comicDiv.find('img')
+
+    # Then extract the source attribute ... 
+    comicSrc = 'https:' + comicImg['src']
+
+    comicFile = '.\\xkcdImages\\xkcd_' + str(comicNumber) + '.png'
+
+    # Then retrieve the image:
+    urllib.request.urlretrieve(comicSrc, comicFile)
 
 # ping
 url = 'https://xkcd.com/'
@@ -21,28 +40,14 @@ while comicNumber > 2332:
     response = requests.get(url) 
     
     try:
-        response.raise_for_status()
-
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-
-        # Identify the div that contains the comic, by id='comic' ...
-        comicDiv = soup.find('div', {'id':'comic'})
-
-        # Then find the img tag within that div ... 
-        comicImg = comicDiv.find('img')
-
-        # Then extract the source attribute ... 
-        comicSrc = 'https:' + comicImg['src']
-
-        comicFile = '.\\xkcdImages\\xkcd_' + str(comicNumber) + '.png'
-
-        # Then retrieve the image:
-        urllib.request.urlretrieve(comicSrc, comicFile)
+        getImage(response)
 
         # This works for one image, now I need to change the url to the previous image, 
         # continually until we're all out of images. 
 
     except:
         print('Invalid response status, %s' % (response.status_code))
+
+    # Here I need a new request for the previous comic
 
     comicNumber -= 1
