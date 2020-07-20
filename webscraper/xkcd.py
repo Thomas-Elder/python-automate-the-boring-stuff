98bf466e356f6deb3643364c489f5a01e5b5e615
Thomas <thombsaway@gmail.com>
2020-07-20 20:20:42 +1000

    Refactored getImage and getPreviousUrl

--- webscraper/xkcd.py
+++ webscraper/xkcd.py
@@ -10,7 +10,18 @@ import urllib.request
 # logging - filename='..\\logs\\xkcd.log', 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def getImage(soup):  
+def getImage(url) -> str:
+    ''' getImage takes a URL for xkcd.com, downloads and saves that image.'''
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except:
+        print('Invalid response status, %s' % (response.status_code))
+
+    # Get soup object to parse the response
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
     # Identify the div that contains the comic, by id='comic' ...
     comicDiv = soup.find('div', {'id':'comic'})
 
@@ -25,9 +36,19 @@ def getImage(soup):
     # Then retrieve the image:
     urllib.request.urlretrieve(comicSrc, comicFile)
 
-def getPreviousPage(soup):
-    # Use this response to find the 'prev' button, and return that url, so it can be passed to the next getImage call
-    
+def getPreviousPage(url) -> str:
+    '''getPreviousPage parses the response from the url to find the 'prev' button
+    and return the url to the previous comic. '''
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except:
+        print('Invalid response status, %s' % (response.status_code))
+
+    # Get soup object to parse the response
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
     # Get the ul for the nav, using the class
     previous = soup.find('a', {'rel':'prev'})
 
@@ -44,19 +65,7 @@ if not os.path.exists('.\\xkcdImages'):
 
 while comicNumber > 2330:
     url = StartingUrl
-    response = requests.get(url) 
-    
-    try:
-        response.raise_for_status()
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-        getImage(soup)
-
-        prev = getPreviousPage(soup)
-
-    except:
-        print('Invalid response status, %s' % (response.status_code))
-
-    # Here I need a new request for the previous comic
-
+    getImage(url)
+    prev = getPreviousPage(url)
     url = prev
     comicNumber -= 1
